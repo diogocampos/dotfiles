37325172edd2ac8e1904ebf66cecf3293e4e33cc
Diogo Campos <diogocampos@users.noreply.github.com>
2020-12-17 22:47:21 -0300

    Add a way to disable announce-time.sh

--- _bin/announce-time.sh
+++ _bin/announce-time.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-time_volume='31'
-prev_volume="`osascript -e 'output volume of (get volume settings)'`"
+[ -f ~/.no-announce-time ] && exit
 
-osascript -e "set volume output volume ${time_volume}"
+speech_volume='31'
+current_volume="`osascript -e 'output volume of (get volume settings)'`"
+
+osascript -e "set volume output volume ${speech_volume}"
 say -v Thomas "Il est `date +'%H:%M'`."
-osascript -e "set volume output volume ${prev_volume}"
+osascript -e "set volume output volume ${current_volume}"
