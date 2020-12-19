97d0f65f86e33fa5f7ad552fba78fabed7ca29d8
Diogo Campos <diogocampos@users.noreply.github.com>
2020-12-18 21:19:02 -0300

    Switch announce-time.sh to Japanese

--- _bin/announce-time.sh
+++ _bin/announce-time.sh
@@ -6,5 +6,11 @@ speech_volume='31'
 current_volume="`osascript -e 'output volume of (get volume settings)'`"
 
 osascript -e "set volume output volume ${speech_volume}"
-say -v Thomas "Il est `date +'%H:%M'`."
+
+# Français
+#say -v Thomas "Il est `date +'%H:%M'`."
+
+# 日本語
+say -v Otoya "今は`date +'%H:%M'`です。"
+
 osascript -e "set volume output volume ${current_volume}"
