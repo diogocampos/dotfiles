5d82a581e0eff478200eefd232d13dcafdef4789
Diogo Campos <diogocampos@users.noreply.github.com>
2020-12-30 23:15:41 -0300

    Add script to generate variations when announcing the time in Japanese

--- _bin/announce-time.sh
+++ _bin/announce-time.sh
@@ -2,15 +2,27 @@
 
 [ -f ~/.no-announce-time ] && exit
 
-speech_volume='31'
-current_volume="`osascript -e 'output volume of (get volume settings)'`"
+node () {
+  local version=default
+  while [ -f ~/.nvm/alias/"$version" ] ; do
+    version="$(cat ~/.nvm/alias/"$version")"
+  done
+  local node=~/.nvm/versions/node/"$version"/bin/node
+  "$node" "$@"
+}
 
+dir="$(dirname "$0")"
+
+speech_volume='31'
+previous_volume="`osascript -e 'output volume of (get volume settings)'`"
 osascript -e "set volume output volume ${speech_volume}"
 
 # Français
 #say -v Thomas "Il est `date +'%H:%M'`."
 
 # 日本語
-say -v Otoya "今は`date +'%H:%M'`です。"
+#say -v Otoya "今は`date +'%H:%M'`です。"
+#NODE_VERSION=default ~/.nvm/nvm-exec node "$dir"/jp-time.js | say -v Otoya
+node "$dir"/jp-time.js | say -v Otoya
 
-osascript -e "set volume output volume ${current_volume}"
+osascript -e "set volume output volume ${previous_volume}"
