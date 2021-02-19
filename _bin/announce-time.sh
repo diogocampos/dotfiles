a1dc798cc0a130355ea2419caf5e70baf60ada16
Diogo Campos <diogocampos@users.noreply.github.com>
2021-02-19 18:42:42 -0300

    Don't announce the time when using external displays

--- _bin/announce-time.sh
+++ _bin/announce-time.sh
@@ -2,16 +2,19 @@
 
 [ -f ~/.no-announce-time ] && exit
 
+displays="$(osascript -e 'tell application "Image Events" to count displays')"
+[ "$displays" -gt 1 ] && exit
+
 dir="$(dirname "$0")"
 
 volume='38'
-prev_volume="`osascript -e 'output volume of (get volume settings)'`"
+prev_volume="$(osascript -e 'output volume of (get volume settings)')"
 if [ "$prev_volume" -gt "$volume" ] ; then
   osascript -e "set volume output volume ${volume}"
 fi
 
 # Français
-#say -v Thomas "Il est `date +'%H:%M'`."
+#say -v Thomas "Il est $(date +'%H:%M')."
 
 # 日本語
 "$dir"/nanji.py | say -v Otoya
